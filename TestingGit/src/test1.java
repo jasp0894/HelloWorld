ebb48a3dee2471f9b802fc2d7bc407a3b35a3927
ic070_23 <ic070_23@aguacate.ece.uprm.edu>
2016-09-19 15:18:43 -0400

    talking shiet part II

--- TestingGit/src/test1.java
+++ TestingGit/src/test1.java
@@ -3,6 +3,6 @@ public class test1 {
 	
 	public static void main(String[] args){
 		System.out.println("Hello   Git !\n");
-		System.out.println("kadngajdbglajhdfgvbajhdgv");
+		System.out.println("kadngajdbglajhdfgvxcnxvnbajhdgv");
 	}
 }
