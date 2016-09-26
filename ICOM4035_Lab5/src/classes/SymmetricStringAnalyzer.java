4a9a14ae111a5c002e787bf9b78925e53dabf8ee
ic070_23 <ic070_23@anon.ece.uprm.edu>
2016-09-26 15:23:24 -0400

    implementing isValidContent().... part 1

--- ICOM4035_Lab5/src/classes/SymmetricStringAnalyzer.java
+++ ICOM4035_Lab5/src/classes/SymmetricStringAnalyzer.java
@@ -1,31 +1,47 @@
 package classes;
 
 public class SymmetricStringAnalyzer {
-	private String s; 
+	private String s;
+
 	public SymmetricStringAnalyzer(String s) {
-		this.s = s; 
+		this.s = s;
 	}
-	
+
 	/**
 	 * Determines if the string s is symmetric
 	 * @return true if it is; false, otherwise. 
 	 */
 	public boolean isValidContent() { 
-		// ADD MISSING CODE
+		LLStack<Character> stack = new LLStack<Character>(); 
+	    for (int i=0; i < s.length(); i++) { 
+	        char c = s.charAt(i); 
+	        if (Character.isLetter(c))
+	           if (Character.isUpperCase(c))
+	              stack.push(c); 
+	           else if (stack.isEmpty())
+	                 return false; 
+	            else {
+	                 char t = stack.top(); 
+	                 if (t == Character.toUpperCase(c))
+	                    stack.pop();  
+	                 else 
+	                    return false; 
+	                }
+	            else 
+	                return false; 
+	    } 
+	    return true; 
 		
-		return true;  // need to change if necessary....
-	}
-	
-	public String toString() { 
-		return s; 
 	}
 
-	public String parenthesizedExpression() 
-	throws StringIsNotSymmetricException 
-	{
+	public String toString() {
+		return s;
+	}
+
+	public String parenthesizedExpression() throws StringIsNotSymmetricException {
 		// ADD MISSING CODE
-		
-		return null;  // need to change if necessary....
+
+		return null; // need to change if necessary....
 	}
 
 }
